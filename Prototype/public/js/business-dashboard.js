89a6b9f157f8e14f23b0295f16613895286306e7
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-02-27 16:51:18 -0800

    Added input validation for business dashboard

---
+++ Prototype/public/js/business-dashboard.js
@@ -0,0 +1,18 @@
+var colorWell;
+var defaultColor = "#0000ff";
+
+window.addEventListener("load", startup, false);
+function startup() {
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  colorWell.addEventListener("change", updateAll, false);
+  colorWell.select();
+}
+function updateFirst(event) {
+  
+    document.body.style.backgroundColor = event.target.value;
+  
+}function updateAll(event) {
+    document.body = event.target.value;
+}

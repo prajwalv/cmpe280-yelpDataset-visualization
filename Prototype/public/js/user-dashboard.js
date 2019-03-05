61352ed2eea08b53d148765bbbc92262d4a4aff2
Yadav <priyalagrawal0810@gmail.com>
2019-03-04 17:44:06 -0800

    Selectable widget added in user dashboard

--- Prototype/public/js/user-dashboard.js
+++ Prototype/public/js/user-dashboard.js
@@ -1,18 +1,30 @@
 var colorWell;
 var defaultColor = "#0000ff";
 
+$(function() {
+  $("#tabs").tabs();
+});
+$(function() {
+  $("#dob").datepicker();
+});
+$(function() {
+  $("#selectable").selectable({
+    cancel: "a,input"
+  });
+});
+
 window.addEventListener("load", startup, false);
 function startup() {
-  colorWell = document.querySelector("#colorWell");
-  colorWell.value = defaultColor;
-  colorWell.addEventListener("input", updateFirst, false);
-  colorWell.addEventListener("change", updateAll, false);
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  colorWell.addEventListener("change", updateAll, false);
   colorWell.select();
 }
+``;
 function updateFirst(event) {
-  
-    document.body.style.backgroundColor = event.target.value;
-  
-}function updateAll(event) {
-    document.body = event.target.value;
+  document.body.style.backgroundColor = event.target.value;
+}
+function updateAll(event) {
+  document.body = event.target.value;
 }

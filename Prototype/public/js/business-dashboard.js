8e83869c89f2983890baf80b76757fd9a5c84c57
Yadav <priyalagrawal0810@gmail.com>
2019-03-08 14:32:34 -0800

    business dashboard left panel changes

--- Prototype/public/js/business-dashboard.js
+++ Prototype/public/js/business-dashboard.js
@@ -1,18 +1,23 @@
 var colorWell;
 var defaultColor = "#0000ff";
 
+$(function() {
+  $("#selectable").selectable({
+    cancel: "a,input"
+  });
+});
+
 window.addEventListener("load", startup, false);
 function startup() {
-  colorWell = document.querySelector("#colorWell");
-  colorWell.value = defaultColor;
-  colorWell.addEventListener("input", updateFirst, false);
-  colorWell.addEventListener("change", updateAll, false);
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  colorWell.addEventListener("change", updateAll, false);
   colorWell.select();
 }
 function updateFirst(event) {
-  
-    document.body.style.backgroundColor = event.target.value;
-  
-}function updateAll(event) {
-    document.body = event.target.value;
+  document.body.style.backgroundColor = event.target.value;
+}
+function updateAll(event) {
+  document.body = event.target.value;
 }

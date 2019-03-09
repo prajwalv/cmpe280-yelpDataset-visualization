bccd02786f86bf254b23f721d5ad4a069ba9af8a
Akshay Jaiswal <akshay.jaiswal@sjsu.edu>
2019-03-08 16:22:45 -0800

    Fixed sidebar links

--- Prototype/public/js/dragdrop.js
+++ Prototype/public/js/dragdrop.js
@@ -37,3 +37,24 @@ function resetTarget(event, ui)
     $("#target").removeClass("ui-state-highlight")
                 .html("Drop on me");
 }
+
+var colorWell;
+var defaultColor = "#0000ff";
+
+window.addEventListener("load", startup, false);
+function startup() {
+  $("#selectable").selectable({
+    cancel: "a,input"
+  });
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  colorWell.addEventListener("change", updateAll, false);
+  colorWell.select();
+}
+function updateFirst(event) {
+  document.body.style.backgroundColor = event.target.value;
+}
+function updateAll(event) {
+  document.body = event.target.value;
+}

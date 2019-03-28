383fc241a5a243ec4d2169643ea98037f9f12e67
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-03-27 18:26:11 -0700

    Add search functionality

--- Prototype/public/js/admin-dashboard.js
+++ Prototype/public/js/admin-dashboard.js
@@ -18,3 +18,19 @@ function updateFirst(event) {
 function updateAll(event) {
   document.body = event.target.value;
 }
+function search_users() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("search_user");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("user_list");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}

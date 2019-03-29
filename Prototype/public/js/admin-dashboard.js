9f0b2a9cbe898ca88bd73c063561a9ec5e947e94
Yadav <priyalagrawal0810@gmail.com>
2019-03-29 14:50:57 -0700

    Admin dashboard look and feel

--- Prototype/public/js/admin-dashboard.js
+++ Prototype/public/js/admin-dashboard.js
@@ -25,12 +25,12 @@ function search_users() {
   ul = document.getElementById("user_list");
   li = ul.getElementsByTagName("li");
   for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-      } else {
-          li[i].style.display = "none";
-      }
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
 }

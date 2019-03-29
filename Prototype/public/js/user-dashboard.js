85c274baea56c86a4a65f4a857365fb044bc988a
Yadav <priyalagrawal0810@gmail.com>
2019-03-28 20:35:57 -0700

    code cleanup

--- Prototype/public/js/user-dashboard.js
+++ Prototype/public/js/user-dashboard.js
@@ -20,22 +20,11 @@ function startup() {
   colorWell.addEventListener("input", updateFirst, false);
   colorWell.addEventListener("change", updateAll, false);
   colorWell.select();
-}
-``;
+};
 function updateFirst(event) {
-<<<<<<< HEAD
-  
     document.body.style.backgroundColor = event.target.value;
-  
-}function updateAll(event) {
-    document.body = event.target.value;
 }
 
-
-=======
-  document.body.style.backgroundColor = event.target.value;
-}
 function updateAll(event) {
   document.body = event.target.value;
 }
->>>>>>> 61352ed2eea08b53d148765bbbc92262d4a4aff2
